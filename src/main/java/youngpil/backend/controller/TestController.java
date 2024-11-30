c7bf1205b7bf093abf7c366464a32829748be4c6
nobodys5 <samudi2@naver.com>
2024-11-30 15:54:26 +0900

    first commit

---
+++ src/main/java/youngpil/backend/controller/TestController.java
@@ -0,0 +1,19 @@
+package youngpil.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/test")
+public class TestController {
+    
+    @GetMapping("t1")
+    public String test() {
+        return "test";
+    }
+}
